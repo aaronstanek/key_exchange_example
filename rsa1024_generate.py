21170749d7ff41a69cc34cd7b892313b010479a2
Aaron Stanek <amr.stanek@gmail.com>
2020-08-24 10:16:48 -0700

    rsa key generation

---
+++ rsa1024_generate.py
@@ -0,0 +1,29 @@
+import sys
+
+if sys.version_info.major != 3:
+    print("Please use Python version 3")
+    exit(0)
+
+from Crypto.PublicKey import RSA
+import secrets
+
+private_key = RSA.generate(1024,randfunc=secrets.token_bytes)
+private_string = private_key.exportKey().decode("UTF-8")
+
+print("")
+print(private_string)
+
+public_key = private_key.publickey()
+public_string = public_key.exportKey().decode("UTF-8")
+
+print("")
+print(public_string)
+
+with open("rsa1024_private_key.txt","w") as file:
+    file.write(private_string)
+
+with open("rsa1024_public_key.txt","w") as file:
+    file.write(public_string)
+
+print("")
+print("Keys written to files")
